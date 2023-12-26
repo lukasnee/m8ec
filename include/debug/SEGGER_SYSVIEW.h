a743da978dc98201bfae5f07a2116b690f55b26d
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2023-12-26 23:24:59 +0200

    build: relax building when SEGGER_SYSVIEW is off

---
+++ include/debug/SEGGER_SYSVIEW.h
@@ -0,0 +1,6 @@
+#pragma once
+#define SEGGER_SYSVIEW_MarkStart(Id)
+#define SEGGER_SYSVIEW_Mark(Id)
+#define SEGGER_SYSVIEW_MarkStop(Id)
+#define SEGGER_SYSVIEW_RecordEnterISR()
+#define SEGGER_SYSVIEW_RecordExitISR()
