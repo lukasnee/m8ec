961c45eece05a1d2c5323ec663896cd94389a540
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2024-02-19 01:30:33 +0200

    wip: fix SPI1 DMA
    DMA seems to only work with D2 RAM

--- source/periph/Spi1.cpp
+++ source/periph/Spi1.cpp
@@ -12,7 +12,12 @@
 
 #include "stm32xxxx_hal.h"
 
+#include <array>
+
 extern SPI_HandleTypeDef hspi1;
+
+DMA_BUFFER std::array<std::uint8_t, 2 * 1024> spi1_dma_tx_buffer;
+DMA_BUFFER std::array<std::uint8_t, 1024> spi1_dma_rx_buffer;
 
 namespace m8ec::periph {
 
@@ -24,23 +29,45 @@ Spi1 &Spi1::get_instance() {
 bool Spi1::ll_init() { return true; /* handled in main.c */ }
 
 bool Spi1::ll_async_read(std::uint8_t *data, std::size_t size) {
-    if (HAL_OK != HAL_SPI_Receive_IT(&hspi1, data, size)) {
+    if (!data) {
         return false;
     }
+    if (size > spi1_dma_rx_buffer.size()) {
+        return false;
+    }
+    if (HAL_OK != HAL_SPI_Receive_DMA(&hspi1, spi1_dma_rx_buffer.data(), size)) {
+        return false;
+    }
+    std::copy(spi1_dma_rx_buffer.begin(), spi1_dma_rx_buffer.begin() + size, data);
     return true;
 }
 
 bool Spi1::ll_async_write(const std::uint8_t *data, std::size_t size) {
-    if (HAL_OK != HAL_SPI_Transmit_IT(&hspi1, const_cast<std::uint8_t *>(data), size)) {
+    if (!data) {
+        return false;
+    }
+    if (size > spi1_dma_tx_buffer.size()) {
+        return false;
+    }
+    std::copy(data, data + size, spi1_dma_tx_buffer.begin());
+    if (HAL_OK != HAL_SPI_Transmit_DMA(&hspi1, spi1_dma_tx_buffer.data(), size)) {
         return false;
     }
     return true;
 }
 
 bool Spi1::ll_async_read_write(std::uint8_t *rd_data, const std::uint8_t *wr_data, std::size_t size) {
-    if (HAL_OK != HAL_SPI_TransmitReceive_IT(&hspi1, const_cast<std::uint8_t *>(wr_data), rd_data, size)) {
+    if (!rd_data || !wr_data) {
         return false;
     }
+    if (size > spi1_dma_tx_buffer.size() || size > spi1_dma_rx_buffer.size()) {
+        return false;
+    }
+    std::copy(wr_data, wr_data + size, spi1_dma_tx_buffer.begin());
+    if (HAL_OK != HAL_SPI_TransmitReceive_DMA(&hspi1, spi1_dma_tx_buffer.data(), spi1_dma_rx_buffer.data(), size)) {
+        return false;
+    }
+    std::copy(spi1_dma_rx_buffer.begin(), spi1_dma_rx_buffer.begin() + size, rd_data);
     return true;
 }
 
--- source/periph/stm32xxxx_hal.h
+++ source/periph/stm32xxxx_hal.h
@@ -5,3 +5,11 @@
 #elif defined(STM32F4)
 #include "stm32f4xx_hal.h"
 #endif
+
+#if defined( __ICCARM__ )
+  #define DMA_BUFFER \
+      _Pragma("location=\".dma_buffer\"")
+#else
+  #define DMA_BUFFER \
+      __attribute__((section(".dma_buffer")))
+#endif
