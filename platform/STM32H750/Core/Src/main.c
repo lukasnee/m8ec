77b7406f3f239912952ad167ee2e4943b0099357
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-01-27 23:52:44 +0200

    chore: add commented out SCB_EnableDCache where it supposed to be

--- platform/STM32H750/Core/Src/main.c
+++ platform/STM32H750/Core/Src/main.c
@@ -85,7 +85,10 @@ int main(void)
 /* Enable the CPU Cache */
 
   /* Enable I-Cache---------------------------------------------------------*/
-  SCB_EnableICache();
+   SCB_EnableICache();
+
+  /* Enable D-Cache---------------------------------------------------------*/
+//   SCB_EnableDCache();
 
   /* MCU Configuration--------------------------------------------------------*/
 
