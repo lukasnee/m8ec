fe73669575cae36dc6894fab6d9bfdf7ebed0d39
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-04-28 22:41:49 +0300

    style: add 1px gap between lines for trash80_stealth57 font

--- source/config/lv_conf.h
+++ source/config/lv_conf.h
@@ -546,10 +546,10 @@
 
 /* Montserrat fonts with ASCII range and some symbols using bpp = 4
  * https://fonts.google.com/specimen/Montserrat */
-#define LV_FONT_MONTSERRAT_8  1
+#define LV_FONT_MONTSERRAT_8  0
 #define LV_FONT_MONTSERRAT_10 0
 #define LV_FONT_MONTSERRAT_12 0
-#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_14 0
 #define LV_FONT_MONTSERRAT_16 0
 #define LV_FONT_MONTSERRAT_18 0
 #define LV_FONT_MONTSERRAT_20 0
