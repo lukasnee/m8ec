cabdd78604a29794f46efd4f6fb9e8f5dcef3f9e
Lukas Neverauskis <lukas.neverauskis@gmail.com>
2025-08-29 00:39:23 +0300

    build: UART1 update forgotten namespaces

--- include/m8ec/periph/Uart1.hpp
+++ include/m8ec/periph/Uart1.hpp
@@ -14,7 +14,7 @@
 
 namespace m8ec::periph {
 
-class Uart1 : public ln::EventDriven::Stream<ln::EventDriven::StreamType::rw> {
+class Uart1 : public ln::drivers::EventDriven::Stream<ln::drivers::EventDriven::StreamType::rw> {
 
 public:
     static Uart1 &get_instance();
